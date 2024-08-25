24496f34c45e652505917946c936d21c0b649e21
Mikhail Rohacevich <mrohacevich@yandex.ru>
2024-08-25 11:04:10 +0300

    feat: added date and time block

---
+++ components/DateAndTimeBlock.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+const DateAndTimeBlock = () => {
+	const [locale, setLocale] = useState('ru-RU'); // Default to Minsk/Moscow
+
+	useEffect(() => {
+		// Update locale if navigator is available
+		if (typeof navigator !== 'undefined') {
+			console.log(navigator.language);
+
+			setLocale(navigator.language || 'ru-RU');
+		}
+	}, []);
+
+	const now = new Date();
+
+	const time = now.toLocaleTimeString(locale, {
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+
+	const date = new Intl.DateTimeFormat(locale, { dateStyle: "full" }).format(now);
+
+	return (
+		<div className="flex flex-col gap-2">
+			<h1 className="text-4xl font-extrabold lg:text-7xl">
+				{time}
+			</h1>
+			<p className="text-lg font-medium text-sky-1 lg:text-2xl">
+				{date}
+			</p>
+		</div>
+	)
+}
+
+export default DateAndTimeBlock
